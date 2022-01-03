b602afa01bf24bc12dd62a27c171729dcb571dbe
klina916 <klina.tw@yahoo.com.tw>
2022-01-03 22:41:28 +0800

    Update request1.py

--- week-2/request1.py
+++ week-2/request1.py
@@ -1,4 +1,9 @@
 def calculate(min, max):
 # 請用你的程式補完這個函式的區塊
-calculate(1, 3) # 你的程式要能夠計算 1+2+3，最後印出 6 
+    sum=0
+    for i in range(min,max+1):
+        sum+=i
+    print(sum)
+
+calculate(1, 3) # 你的程式要能夠計算 1+2+3，最後印出 6
 calculate(4, 8) # 你的程式要能夠計算 4+5+6+7+8，最後印出 30
