4b5dee80dff862aa2253686c37508c75ae889b87
klina916 <klina.tw@yahoo.com.tw>
2022-01-28 22:48:44 +0800

    Create assignment2.sql

---
+++ week-5/assignment2.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `member`( 
+	`id` BIGINT AUTO_INCREMENT, 
+	`name` VARCHAR(255) NOT NULL, 
+	`username` VARCHAR(255) NOT NULL, 
+	`password` VARCHAR(255) NOT NULL, 
+	`follower_count` INT DEFAULT 0 NOT NULL, 
+	`time` DATETIME NOT NULL DEFAULT (GETDATE()), 
+	PRIMARY KEY(id)
+);
